af5d8786687115ce1387bb7d1c101c49e2fefec2
Jason Huang <cjhuang@yahoo-inc.com>
2017-05-28 17:01:09 +0800

    add index layout

--- src/App.js
+++ src/App.js
@@ -1,38 +1,58 @@
 import React, {Component} from 'react';
-import {Table} from 'antd';
-import logo from './logo.svg';
+import {Table, Button, Tabs, Badge, Tag} from 'antd';
 import './App.css';
+const {TabPane} = Tabs;
 
-const dataSource = [
+const operations = <Button>新增任務</Button>;
+
+const questDtaSource = [
   {
     key: '1',
-    name: '胡彦斌',
-    age: 32,
-    address: '西湖区湖底公园1号',
+    name: '人中之龍6',
+    type: 'buy',
+    platform: 'PS4',
+    aliases: ['人龍6'],
+    unreadPostCount: 8,
   },
   {
     key: '2',
-    name: '胡彦祖',
-    age: 42,
-    address: '西湖区湖底公园1号',
+    name: '鐵拳7',
+    type: 'buy',
+    platform: 'PS4',
+    aliases: ['鐵7'],
+  },
+  {
+    key: '3',
+    name: '鐵拳6',
+    type: 'sell',
+    platform: 'PS4',
+    aliases: ['鐵6'],
   },
 ];
 
 const columns = [
   {
-    title: '姓名',
+    title: '遊戲',
     dataIndex: 'name',
     key: 'name',
+    render: (text, {unreadPostCount}) => (
+      <Badge dot={unreadPostCount}>
+        <span style={{paddingRight: 4}}>{text}</span>
+      </Badge>
+    ),
   },
   {
-    title: '年龄',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: '住址',
-    dataIndex: 'address',
-    key: 'address',
+    title: '平台',
+    dataIndex: 'platform',
+    key: 'platform',
+    render: text => {
+      switch (text) {
+        case 'PS4':
+          return <Tag color="#108ee9">{text}</Tag>;
+        default:
+          return <Tag>{text}</Tag>;
+      }
+    },
   },
 ];
 
@@ -40,14 +60,26 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Table dataSource={dataSource} columns={columns} />
+
+        <Tabs tabBarExtraContent={operations}>
+          <TabPane tab="買遊戲" key="1">
+            <Table
+              dataSource={questDtaSource.filter(row => row.type === 'buy')}
+              columns={columns}
+              pagination={false}
+              size="middle"
+              expandedRowRender={() => <h1>11</h1>}
+            />
+          </TabPane>
+          <TabPane tab="賣遊戲" key="2">
+            <Table
+              dataSource={questDtaSource.filter(row => row.type === 'sell')}
+              columns={columns}
+              pagination={false}
+              size="middle"
+            />
+          </TabPane>
+        </Tabs>
       </div>
     );
   }

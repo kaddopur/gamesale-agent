dccd86b784d031115be9f4e5c00bb1695ce32449
Jason Huang <cjhuang@yahoo-inc.com>
2017-05-28 21:18:20 +0800

    write PostTable using JSX syntax

--- src/components/PostTable.js
+++ src/components/PostTable.js
@@ -2,42 +2,32 @@ import React from 'react';
 import {Table, Badge} from 'antd';
 import Link from './Link';
 
-const postColumns = [
-  {
-    title: '日期',
-    dataIndex: 'date',
-    key: 'date',
-  },
-  {
-    title: '作者',
-    dataIndex: 'author',
-    key: 'author',
-  },
-  {
-    title: '遊戲',
-    dataIndex: 'title',
-    key: 'title',
-    render: (text, {link, read}) => (
-      <Badge dot={read === false}>
-        <Link url={link} style={{paddingRight: 4}}>
-          {text}
-        </Link>
-      </Badge>
-    ),
-  },
-];
+const {Column} = Table;
 
 const PostTable = ({dataSource}) => (
   <Table
     dataSource={dataSource}
-    columns={postColumns}
     showHeader={false}
     pagination={false}
     size="middle"
     locale={{
       emptyText: '暫無數據',
-    }}
-  />
+    }}>
+    <Column title="日期" dataIndex="date" key="date" width="40" />
+    <Column title="作者" dataIndex="author" key="author" width="100" />
+    <Column
+      title="遊戲"
+      dataIndex="title"
+      key="title"
+      render={(text, {link, read}) => (
+        <Badge dot={read === false}>
+          <Link url={link} style={{paddingRight: 4}}>
+            {text}
+          </Link>
+        </Badge>
+      )}
+    />
+  </Table>
 );
 
 export default PostTable;

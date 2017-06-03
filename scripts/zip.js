74aa6b8425fa744a4966dbc54fbecfe1fa7f8787
Jason Huang <cjhuang@yahoo-inc.com>
2017-06-03 15:24:31 +0800

    use zip-folder

--- scripts/zip.js
+++ scripts/zip.js
@@ -1,4 +1,9 @@
-const AdmZip = require('adm-zip');
-const zip = new AdmZip();
-zip.addLocalFolder(`${process.cwd()}/build`);
-zip.writeZip(`${process.cwd()}/bundle.zip`);
+const zipFolder = require('zip-folder');
+
+zipFolder(`${process.cwd()}/build`, `${process.cwd()}/bundle.zip`, err => {
+  if (err) {
+    console.log('oh no!', err);
+  } else {
+    console.log('EXCELLENT');
+  }
+});
